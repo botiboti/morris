3b57a2e8da3f92bd0980ef03f8bb86e3244d14c4
Ferenczi Botond <37540762+botiboti@users.noreply.github.com>
2018-08-05 18:33:29 +0300

    hellobello

--- Main.elm
+++ Main.elm
@@ -1,64 +1,16 @@
-module Malom exposing (..)
+module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import List exposing (..)
 import List.Extra
-
-
-type alias Model =
-    { board : Board
-    , moveinprogress : MoveInProgress
-    , counter : Int
-    }
-
-
-type alias Location =
-    ( Column, Row, Ring )
-
-
-type alias Board =
-    List Player
-
-
-type MoveInProgress
-    = NoClick
-    | FirstClick Location
-    | SecondClick Location Location
-
-
-type Column
-    = X1
-    | X2
-    | X3
-
-
-type Row
-    = Y1
-    | Y2
-    | Y3
-
-
-type Ring
-    = Z1
-    | Z2
-    | Z3
-
-
-type Player
-    = Empty
-    | W
-    | B
+import Types exposing (..)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { board = List.map (always Empty) allLocations, moveinprogress = NoClick, counter = 0 }, Cmd.none )
-
-
-type Msg
-    = Click Location
 
 
 update : Msg -> Model -> Model
@@ -658,68 +610,3 @@ toCheck bool =
 viewAlls : Html Msg
 viewAlls =
     div [] (List.map (\x -> text <| toString x) allLocations)
-
-
-allColumns : List Column
-allColumns =
-    [ X1, X2, X3 ]
-
-
-allRows : List Row
-allRows =
-    [ Y1, Y2, Y3 ]
-
-
-allRings : List Ring
-allRings =
-    [ Z1, Z2, Z3 ]
-
-
-allMills : List (List Location)
-allMills =
-    [ [ ( X1, Y1, Z1 ), ( X2, Y1, Z1 ), ( X3, Y1, Z1 ) ]
-    , [ ( X1, Y1, Z1 ), ( X1, Y2, Z1 ), ( X1, Y3, Z1 ) ]
-    , [ ( X1, Y3, Z1 ), ( X2, Y3, Z1 ), ( X3, Y3, Z1 ) ]
-    , [ ( X3, Y1, Z1 ), ( X3, Y2, Z1 ), ( X3, Y3, Z1 ) ]
-    , [ ( X1, Y1, Z2 ), ( X2, Y1, Z2 ), ( X3, Y1, Z2 ) ]
-    , [ ( X1, Y1, Z2 ), ( X1, Y2, Z2 ), ( X1, Y3, Z2 ) ]
-    , [ ( X1, Y3, Z2 ), ( X2, Y3, Z2 ), ( X3, Y3, Z2 ) ]
-    , [ ( X3, Y1, Z2 ), ( X3, Y2, Z2 ), ( X3, Y3, Z2 ) ]
-    , [ ( X1, Y1, Z3 ), ( X2, Y1, Z3 ), ( X3, Y1, Z3 ) ]
-    , [ ( X1, Y1, Z3 ), ( X1, Y2, Z3 ), ( X1, Y3, Z3 ) ]
-    , [ ( X1, Y3, Z3 ), ( X2, Y3, Z3 ), ( X3, Y3, Z3 ) ]
-    , [ ( X3, Y1, Z3 ), ( X3, Y2, Z3 ), ( X3, Y3, Z3 ) ]
-    , [ ( X2, Y1, Z1 ), ( X2, Y1, Z2 ), ( X2, Y1, Z3 ) ]
-    , [ ( X1, Y2, Z1 ), ( X1, Y2, Z2 ), ( X1, Y2, Z3 ) ]
-    , [ ( X2, Y3, Z1 ), ( X2, Y3, Z2 ), ( X2, Y3, Z3 ) ]
-    , [ ( X3, Y2, Z1 ), ( X3, Y2, Z2 ), ( X3, Y2, Z3 ) ]
-    ]
-
-
-allLocations : List Location
-allLocations =
-    [ ( X1, Y1, Z1 )
-    , ( X2, Y1, Z1 )
-    , ( X3, Y1, Z1 )
-    , ( X1, Y1, Z2 )
-    , ( X2, Y1, Z2 )
-    , ( X3, Y1, Z2 )
-    , ( X1, Y1, Z3 )
-    , ( X2, Y1, Z3 )
-    , ( X3, Y1, Z3 )
-    , ( X1, Y2, Z1 )
-    , ( X1, Y2, Z2 )
-    , ( X1, Y2, Z3 )
-    , ( X3, Y2, Z3 )
-    , ( X3, Y2, Z2 )
-    , ( X3, Y2, Z1 )
-    , ( X1, Y3, Z3 )
-    , ( X2, Y3, Z3 )
-    , ( X3, Y3, Z3 )
-    , ( X1, Y3, Z2 )
-    , ( X2, Y3, Z2 )
-    , ( X3, Y3, Z2 )
-    , ( X1, Y3, Z1 )
-    , ( X2, Y3, Z1 )
-    , ( X3, Y3, Z1 )
-    ]
